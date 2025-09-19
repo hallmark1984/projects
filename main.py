2f4d7b71b120efd5440d599c73b5ada286904513
hallmark1984 <matt.mcaleavy@gmail.com>
2025-09-19 12:59:27 +0000

    start

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    import pygame
+    pygame.init()
+    screen = pygame.display.set_mode((640, 480))
+    pygame.display.set_caption('Pygame Window')
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
